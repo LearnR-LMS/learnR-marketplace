f612abff5b96e78d615afa5fcbd028a743a98db4
Nguyen Dinh Khoi (FTEL ISC HCM) <khoind9@fpt.com.vn>
2022-07-25 16:27:18 +0700

    fix

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,35 +1,31 @@
 import React from 'react'
-import { SigningCosmWasmClient } from '@cosmjs/stargate'
 import { Image, Menu, Button } from 'antd'
 import logo_learnr from '../assets/images/logo_learnr.png'
 
 export default function AppHeader() {
     const connectWallet = (e) => {
-        const chainId = 'halo-testnet-001'
-        window.keplr.enable(chainId)
-            .then(async res => {
-                if (res) {
-                    const offlineSigner = window.getOfflineSigner(chainId)
-                    const cosmwasm = await SigningCosmWasmClient.connectWithSigner('https://rpc.halo.aura.network:443', offlineSigner)
-                    const account = (await offlineSigner.getAccounts())[0]
-                    console.log(account)
-                }
-            }).catch(err => console.log(err));
+        // const chainId = 'halo-testnet-001'
+        // window.keplr.enable(chainId)
+        //     .then(async res => {
+        //         if (res) {
+        //             const offlineSigner = window.getOfflineSigner(chainId)
+        //             const cosmwasm = await SigningCosmWasmClient.connectWithSigner('https://rpc.halo.aura.network:443', offlineSigner)
+        //             const account = (await offlineSigner.getAccounts())[0]
+        //             console.log(account)
+        //         }
+        //     }).catch(err => console.log(err));
     }
     return (
-        <>
-            <div>
-                <Menu>
-                    <div style={{ display: "flex", opacity: "0.5", flexDirection: "row" }}>
-                        <div style={{ flex: "1", justifyContent: "center", alignItems: "center", flexDirection: "row", display: "flex", }}>
-                            <Menu.Item><Image src={logo_learnr} /></Menu.Item>
-                            <Menu.Item>Profile</Menu.Item>
-                            <Menu.Item>Inventory</Menu.Item>
-                        </div>
-                        <Button onClick={connectWallet}>Login</Button>
+        <div>
+            <Menu>
+                <div style={{ display: "flex", opacity: "0.5", flexDirection: "row" }}>
+                    <div style={{ flex: "1", justifyContent: "center", alignItems: "center", flexDirection: "row", display: "flex", }}>
+                        <Menu.Item>Profile</Menu.Item>
+                        <Menu.Item>Inventory</Menu.Item>
                     </div>
-                </Menu >
-            </div >
-        </>
+                    <Button onClick={connectWallet}>Login</Button>
+                </div>
+            </Menu >
+        </div >
     )
 }
